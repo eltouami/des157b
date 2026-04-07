863de2f98ebe1da22712333a8c04201008026df0
elly_touami <eltouami@ucdavis.edu>
2026-04-07 11:45:10 -0700

    portal edits

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 (function() {
     'use strict';
 
-    const button = document.querySelector('button');
+    const flip = document.querySelector('.basket-wrap');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
     const sections = document.querySelectorAll('section')
     let mode = 'dark';
 
-    button.addEventListener('click', function() {
+    flip.addEventListener('click', function() {
         if (mode === 'dark') {
             body.className = 'switch';
             banner.className = 'switch';
-            button.className = 'switch';
+            flip.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -19,7 +19,7 @@
         } else {
             body.removeAttribute('class');
             banner.removeAttribute('class');
-            button.removeAttribute('class');
+            flip.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
             }
