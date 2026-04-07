79687d77a1a890752ef1b72764ed1822b1a43acc
elly_touami <eltouami@ucdavis.edu>
2026-04-07 12:56:34 -0700

    new changes

--- script.js
+++ script.js
@@ -4,26 +4,31 @@
     const flip = document.querySelector('.basket-wrap');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const sections = document.querySelectorAll('section');
+    const stars = document.querySelectorAll('.star');
+    const stickers = document.querySelectorAll('.sticker');
+    const basketImg = document.querySelector('.basket');
     let mode = 'dark';
 
     flip.addEventListener('click', function() {
         if (mode === 'dark') {
-            body.className = 'switch';
-            banner.className = 'switch';
-            flip.className = 'switch';
-            for (const section of sections) {
-                section.className = 'switch';
-            }
+            body.classList.add('switch');
+            banner.classList.add('switch');
+            flip.classList.add('switch');
+            sections.forEach(s => s.classList.add('switch'));
+            stars.forEach(s => s.classList.add('switch'));
+            stickers.forEach(s => s.classList.add('switch'));
+            basketImg.src = 'images/basket-black2.png';
             mode = 'light';
         } else {
-            body.removeAttribute('class');
-            banner.removeAttribute('class');
-            flip.removeAttribute('class');
-            for (const section of sections) {
-                section.removeAttribute('class');
-            }
-            mode = 'dark'
+            body.classList.remove('switch');
+            banner.classList.remove('switch');
+            flip.classList.remove('switch');
+            sections.forEach(s => s.classList.remove('switch'));
+            stars.forEach(s => s.classList.remove('switch'));
+            stickers.forEach(s => s.classList.remove('switch'));
+            basketImg.src = 'images/basket-white2.png';
+            mode = 'dark';
         }
-    })
-})()
+    });
+})();
